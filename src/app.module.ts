528ffc76b632b8d3c1579de5d22be24e18f1fe6b
Hyeonseong0917 <sagiri0917@gmail.com>
2026-02-23 00:33:33 +0900

    feat: connect TypeORM with MySQL

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,31 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TodosModule } from './todos/todos.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Todo } from './todos/entities/todo.entity';
 
 @Module({
-  imports: [TodosModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'mysql',
+        host: configService.get('DB_HOST'),
+        port: configService.get<number>('DB_PORT'),
+        username: configService.get('DB_USERNAME'),
+        password: configService.get('DB_PASSWORD'),
+        database: configService.get('DB_DATABASE'),
+        entities: [Todo],
+        synchronize: true,
+      }),
+    }),
+    TodosModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/todos/todos.service.ts
+++ src/todos/todos.service.ts
@@ -1,44 +1,41 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
-import {CreateTodoDto} from './dto/create-todo.dto';
-import {UpdateTodoDto} from './dto/update-todo.dto';
-import {Todo} from './entities/todo.entity';
-
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { CreateTodoDto } from './dto/create-todo.dto';
+import { UpdateTodoDto } from './dto/update-todo.dto';
+import { Todo } from './entities/todo.entity';
 
 @Injectable()
 export class TodosService {
-    private todos: Todo[]=[];
-    private nextId=1;
-    create(createTodoDto: CreateTodoDto): Todo{
-        const todo: Todo={
-            id: this.nextId++,
-            title: createTodoDto.title,
-            description: createTodoDto.description,
-            isCompleted: false,
-            createdAt: new Date(),
-        };
-        this.todos.push(todo);
-        return todo;
-    }
-    findAll(): Todo[]{
-        return this.todos;
-    }
-    findOne(id: number): Todo{
-        const todo=this.todos.find(t=>t.id===id);
-        if(!todo) throw new NotFoundException(`${id}번 Todo가 없습니다.`);
-        return todo;
-    }
-    update(id: number, updateTodoDto: UpdateTodoDto){
-        const todo=this.findOne(id);
-        Object.assign(todo, updateTodoDto);
-        return todo;
-    }
-    remove(id: number){
-        // id인거 삭제
-        const index=this.todos.findIndex((t)=>t.id===id);
-        if(index===-1) throw new NotFoundException(`${id}번 Todo가 없습니다.`);
-        this.todos.splice(index,1);
-        return {message: "삭제되었습니다."};
-    }
+  constructor(
+    @InjectRepository(Todo)
+    private todoRepository: Repository<Todo>,
+  ) {}
 
+  async create(createTodoDto: CreateTodoDto): Promise<Todo> {
+    const todo = this.todoRepository.create(createTodoDto);
+    return await this.todoRepository.save(todo);
+  }
 
+  async findAll(): Promise<Todo[]> {
+    return await this.todoRepository.find();
+  }
+
+  async findOne(id: number): Promise<Todo> {
+    const todo = await this.todoRepository.findOne({ where: { id } });
+    if (!todo) throw new NotFoundException(`${id}번 Todo가 없습니다.`);
+    return todo;
+  }
+
+  async update(id: number, updateTodoDto: UpdateTodoDto): Promise<Todo> {
+    const todo = await this.findOne(id);
+    Object.assign(todo, updateTodoDto);
+    return await this.todoRepository.save(todo);
+  }
+
+  async remove(id: number): Promise<{ message: string }> {
+    const todo = await this.findOne(id);
+    await this.todoRepository.remove(todo);
+    return { message: '삭제되었습니다.' };
+  }
 }
