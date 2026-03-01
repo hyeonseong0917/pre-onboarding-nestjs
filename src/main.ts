89c31aa62a0ed3edb96ab0dc44c8cf5b78d00d16
Hyeonseong0917 <sagiri0917@gmail.com>
2026-03-01 13:45:31 +0900

    feat: Add Swagger API Docs

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,8 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import {HttpExceptionFilter} from './common/filters/http-exception.filter';
 import {TransformInterceptor} from './common/interceptors/transform.interceptor';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -15,6 +17,17 @@ async function bootstrap() {
   );
   app.useGlobalFilters(new HttpExceptionFilter());
   app.useGlobalInterceptors(new TransformInterceptor());
+
+  const config=new DocumentBuilder()
+  .setTitle('Todo API')
+  .setDescription('NestJS Todo List API 문서')
+  .setVersion('1.0')
+  .addBearerAuth()
+  .build();
+
+  const document=SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api',app,document);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
--- src/todos/dto/create-todo.dto.ts
+++ src/todos/dto/create-todo.dto.ts
@@ -1,10 +1,13 @@
+import { ApiProperty } from '@nestjs/swagger';
 import {IsString, IsNotEmpty, IsOptional} from 'class-validator';
 
 export class CreateTodoDto{
+    @ApiProperty({example: '운동하기', description: 'Todo 제목'})
     @IsNotEmpty()
     @IsString()
     title: string;
 
+    @ApiProperty({example: '헬스장 1시간', description: 'Todo 설명', required: false})
     @IsOptional()
     @IsString()
     description: string;
